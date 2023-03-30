9e973720e1e36d7defdddf71de85fd95fd91a4b3
Marija K <kccmrj@outlook.com>
2023-03-30 15:39:20 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,10 @@
 const User = require("../models/user");
 const jwt = require('jsonwebtoken');
 
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
+
 const user_login = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -8,9 +12,7 @@ const user_login = async (req, res) => {
    const id = user._id;
    const token = createToken(id);
    const accountType = user.accountType;
-   res
-    .status(200)
-    .json({ token, accountType });
+   res.status(200).json({ token, accountType, redirect: "/blogs" })
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
