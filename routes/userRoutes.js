a9a2122ab92a6c76071db7b150a08b98c05433cc
Marija K <kccmrj@outlook.com>
2023-04-03 22:16:04 +0200

    added logout route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/login", authController.user_login);
+router.get("/logout", authController.user_logout);
 
 module.exports = router;
