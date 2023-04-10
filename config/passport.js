d05ca8d5c039af6f05dff6ae30d6398db94ebba7
Marija K <kccmrj@outlook.com>
2023-04-10 22:02:11 +0200

    adding passport configuration

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const { verifyPassword } = require("../utils/passwordUtils");
+const LocalStrategy = require("passport-local").Strategy;
+const connection = require('./database');
+const User = connection.models.OtherUser;
+
+// to verify credentials, passport will look for "username" and "password" literally, but this can be modified
+// by setting up custom fields:
+const customFields = {
+    usernameField: "email",
+    passwordField: "password"
+}
+
+
+// this is reminiscent of static methods:
+const verifyCallback = async (username, password, done) => { // done is a function you eventually pass the result of your auth to
+ try {
+   const user = await User.findOne({ username: username });
+   if(!user){
+      return done(null, false) // error:null, res:false
+   }
+   const isValid = await verifyPassword(password, user.hash, user.salt);
+   if(isValid){
+      return done(null, user)
+   } else {
+      return done(null, false)
+   }  
+ } catch (error) {
+    done(error);
+ }
+}
+
+const strategy = new LocalStrategy(customFields, verifyCallback); // customFields are passed here as the 1st arg 
+
+passport.use(strategy)
+
