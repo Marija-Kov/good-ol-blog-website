0895a5b06557880301b3785611e51e1bd2f25d1c
Marija K <kccmrj@outlook.com>
2023-04-10 22:22:28 +0200

    Update app.js
    - Require, configure and register passport;
    
    - Require express-session and connect-mongo;
    
    - Create mongooseConnection with createConnection (returns a connection instead of promise);
    
    - Create session storage with connect-mongo storage constructor (MongoStore);
    
    - Register the session;
    
    - Listen on port;

--- app.js
+++ app.js
@@ -6,17 +6,29 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 const expressEjsLayouts = require('express-ejs-layouts');
 require('dotenv').config(); 
+const passport = require("passport");
+
+// SESSION STORAGE
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 mongoose.set("strictQuery", false);
 
 app.use(express.json());  
 
 const dbURI = process.env.MONGO_URI;  
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })  
-     .then(result => app.listen(3002))
-     .then(()=>console.log('connected'))
-     .catch(error=> console.log(`Error: ${error}`));
+const mongooseConnection = mongoose.createConnection(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+  //  mongoose.connect(dbURI, {
+  //    useNewUrlParser: true,
+  //    useUnifiedTopology: true,
+  //  })
+  // .then((result) => app.listen(process.env.PORT))
+  // .then(() => console.log("connected"))
+  // .catch((error) => console.log(`Error: ${error}`));
 
 app.set('view engine', 'ejs');  
                           
@@ -24,6 +36,21 @@ app.use(expressEjsLayouts);
 app.use(express.static('public'));  
 app.use(morgan('dev')); 
 app.use(express.urlencoded({ extended: true }));
+
+const sessionStore = new MongoStore({
+  mongooseConnection: mongooseConnection,
+  collection: 'sessions'
+});
+
+app.use(
+      session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: true,
+      store: sessionStore,
+      cookie: {maxAge: 360000}
+    })
+   )
 
 app.use((req, res, next) => {
   if(req.headers.cookie){
@@ -39,12 +66,16 @@ app.use((req, res, next) => {
   }
   next();
 });
-/// +++++++++++++++++++++++++++++++++
+
+// REGISTER PASSPORT
+require('./config/passport');
+// keep reinitializing passport middleware as we hit different routes
+app.use(passport.initialize());
+app.use(passport.session())
 
 app.get('/', (req, res) => {  
     res.redirect('/blogs'); 
 })                         
-
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" }); 
 });
@@ -61,5 +92,11 @@ app.use("/user", userRoutes);
 app.use((req, res) => { 
     res.status(404).render("404", { title: "Page Not Found" });
 })
+
+function listen(){
+  app.listen(process.env.PORT)
+  console.log('listening')
+}
+listen()
 
 module.exports = app
