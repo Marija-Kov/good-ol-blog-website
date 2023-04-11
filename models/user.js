b9e1ceebd5eac206a03e1ebff042ef84251306b6
Marija K <kccmrj@outlook.com>
2023-04-11 20:11:06 +0200

    refactoring to export userSchema

--- models/user.js
+++ models/user.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// const bcrypt = require("bcrypt");
-
 
 const userSchema = new Schema({
   email: {
     type: String,
-    required: [true, "Please provide an email."],
     unique: true,
   },
-  password: {
-    type: String,
-    required: [true, "Please provide a password."],
-  },
+  hash: String,
+  salt: String,
   accountType: {
     type: String,
     enum: ["admin", "other"],
@@ -20,33 +15,5 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.statics.login = async function (email, password) {
-  if (!email || !password) {
-    throw Error("All fields are required");
-  }
-  if (
-    !email.match(
-      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    )
-  ) {
-    throw Error("Invalid email address");
-  }
-  const user = await this.findOne({ email });
-  if (!user) {
-    throw Error("That email does not exist in our database");
-  }
 
-  // const match = await bcrypt.compare(password, user.password);
-  // if (!match) {
-  //   throw Error("Wrong password");
-  // }
- 
-  if(password !== user.password){
-    throw Error("Wrong password");
-  }
-
-  return user;
-};
-
-const User = mongoose.model("User",userSchema); 
-module.exports = User
+module.exports = userSchema;
