064c2ce32171e9b3590896be5430e22089489c60
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-11-27 22:59:35 +0100

    Refactored routeCache middleware

--- routeCache.js
+++ routeCache.js
@@ -1,28 +1,36 @@
 import NodeCache from "node-cache";
-const cache = new NodeCache();
+class RouteCache {
+  constructor() {
+    this.cache = new NodeCache();
+    this.middleware = this.middleware.bind(this);
+    this.flush = this.flush.bind(this);
+  }
+  middleware(duration) {
+    return (req, res, next) => {
+      if (req.method !== "GET") {
+        console.error("Only GET methods can be cached");
+        return next();
+      }
+      const key = req.originalUrl;
+      const cachedResponse = this.cache.get(key);
+      if (cachedResponse) {
+        console.log(`Cache hit for ${key}`);
+        res.send(cachedResponse);
+      } else {
+        console.log(`Cache miss for ${key}`);
+        res.originalSend = res.send;
+        res.send = (body) => {
+          res.originalSend(body);
+          this.cache.set(key, body, duration);
+        };
+        next();
+      }
+    };
+  }
 
-const routeCache = (duration) => {
-  return (req, res, next) => {
-    if (req.method !== "GET") {
-      console.error("Only GET methods can be cached");
-      return next();
-    }
-    const key = req.originalUrl;
-    const cachedResponse = cache.get(key);
-    if (cachedResponse) {
-      console.log(`Cache hit for ${key}`);
-      res.send(cachedResponse);
-    } else {
-      console.log(`Cache miss for ${key}`);
-      res.originalSend = res.send;
-      res.send = (body) => {
-        console.log(body)
-        res.originalSend(body);
-        cache.set(key, body, duration);
-      };
-      next();
-    }
-  };
-};
+  flush() {
+    this.cache.flushAll();
+  }
+}
 
-export default routeCache;
+export default new RouteCache();
